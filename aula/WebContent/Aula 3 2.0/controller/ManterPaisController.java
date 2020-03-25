a00be4c4f142c267193a423edd4fb6ae077634dc
Vinicius <Vinicius@DESKTOP-60K4ICB>
2020-03-25 18:58:03 -0300

    aula 03

---
+++ aula/WebContent/Aula 3 2.0/controller/ManterPaisController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pais;
+import service.PaisService;
+
+/**
+ * Servlet implementation class ManterPaisController
+ */
+@WebServlet("/ManterPais.do")
+public class ManterPaisController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String pNome = request.getParameter("nome");
+		long pPopulacao = Long.parseLong(request.getParameter("populacao"));
+		Double pArea = Double.parseDouble(request.getParameter("area"));
+		
+		//instanciar o javabean
+		Pais pais = new Pais();
+		pais.setNome(pNome);
+		pais.setPopulacao(pPopulacao);
+		pais.setArea(pArea);
+		
+		//instanciar o service
+		PaisService cs = new PaisService();
+		cs.criar(pais);
+		pais = cs.carregar(pais.getId());
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Cliente Cadastrado</title></head><body>");
+		out.println(	"id: "+pais.getId()+"<br>");
+		out.println(	"nome: "+pais.getNome()+"<br>");
+		out.println(	"fone: "+pais.getPopulacao()+"<br>");
+		out.println(	"e-mail: "+pais.getArea()+"<br>");
+	    out.println("</body></html>");
+		
+	}
+
+}
